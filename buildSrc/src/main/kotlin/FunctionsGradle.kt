cad0f61550a49adc2a661d9dcfae13599da9c572
Francisco Javier Avilés <franciscojavieravifer@gmail.com>
2022-03-03 20:48:18 +0100

    Fix bugs in FunctionsGradle.kt

--- buildSrc/src/main/kotlin/FunctionsGradle.kt
+++ buildSrc/src/main/kotlin/FunctionsGradle.kt
@@ -3,6 +3,9 @@ import java.util.*
 
 object FunctionsGradle {
 
+    private const val DIR_PRIVATE_DATA = "privateData"
+    private const val FINAL_FILE_PROPERTIES = "SignConfig.properties"
+
     private const val PATH_FILE = "PATH_FILE"
     private const val STORE_PASSWORD = "STORE_PASSWORD"
     private const val KEY_ALIAS = "KEY_ALIAS"
@@ -12,15 +15,21 @@ object FunctionsGradle {
         RELEASE, DEBUG;
 
         fun getPathFile() =
-            "privateData/${this.name.toLowerCase(Locale.ROOT)}SignConfig.properties"
+            getAbsolutePath() + "/" + DIR_PRIVATE_DATA + "/" + name.toLowerCase(Locale.ROOT) + FINAL_FILE_PROPERTIES
 
     }
+
+    private fun getAbsolutePath() = System.getProperty("user.dir")
 
     private fun getPropertiesFile(signConfig: SignConfig): Properties =
         Properties().apply { load(File(signConfig.getPathFile()).inputStream()) }
 
     fun getFileStore(signConfig: SignConfig): File =
-        File("privateData/${getPropertiesFile(signConfig).getProperty(PATH_FILE)}")
+        File(
+            getAbsolutePath() + "/" + DIR_PRIVATE_DATA + "/" + getPropertiesFile(signConfig).getProperty(
+                PATH_FILE
+            )
+        )
 
     fun getStorePassword(signConfig: SignConfig): String =
         getPropertiesFile(signConfig).getProperty(STORE_PASSWORD)
